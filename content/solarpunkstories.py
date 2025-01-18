713aeff5ff85d9bbf1431d10caa01fb871ee4b88
MessinaSystems <ryan.messina@messinasystems.com>
2025-01-18 20:02:21 +1100

    updated to json format

--- content/solarpunkstories.py
+++ content/solarpunkstories.py
@@ -1,3 +1,18 @@
-characters = {"me": "A person who enjoys things"}
-locations = {"city": "highly dense population"}
+import json
 
+characters = {}
+locations = {}
+technology = {}
+
+with open("character.json","r") as file:
+    characters = json.load(file)
+
+with open("locations.json","r") as file:
+    locations = json.load(file)
+
+with open("technology.json","r") as file:
+    technology = json.load(file)
+
+
+#"city": "highly dense population"
+#"me": "A person who enjoys things"
