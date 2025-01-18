381bcd7f93a0213ea8a2889d8250eb989544c651
MessinaSystems <ryan.messina@messinasystems.com>
2025-01-18 20:13:03 +1100

    typos

--- content/solarpunkstories.py
+++ content/solarpunkstories.py
@@ -4,10 +4,10 @@ characters = {}
 regions = {}
 technology = {}
 
-with open("character.json","r") as file:
+with open("charactere.json","r") as file:
     characters = json.load(file)
 
-with open("locations.json","r") as file:
+with open("regions.json","r") as file:
     regions = json.load(file)
 
 with open("technology.json","r") as file:
